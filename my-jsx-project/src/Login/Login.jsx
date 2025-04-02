c6b5696e8c9e6d1d95bf0825dc698849773ebd60
EneAndrei789 <andreileonardene@gmail.com>
2025-04-02 20:24:20 +0300

    mid styles login process

--- my-jsx-project/src/Login/Login.jsx
+++ my-jsx-project/src/Login/Login.jsx
@@ -7,14 +7,13 @@ const Login = () => {
             <div className="LoginForm">
                 <form action="">
                     <label htmlFor="Email"></label>
-                    <input className="LoginEmailInput LoginInput" type="text" id="LoginFormEmail" name="Email" placeholder="Email" style={{ marginBottom: "10px" }} /> <br />
+                    <input className="LoginEmailInput LoginInput" type="text" id="LoginFormEmail" name="Email" placeholder="Email" style={{ marginBottom: "10px", background: "transparent"}} /> <br />
                     <label htmlFor="Password"></label>
-                    <input className="LoginPasswordInput LoginInput" type="text" id="LoginFormPassword" name="Password" placeholder="Password" style={{ marginBottom: "10px" }} /> <br />
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <button>Login</button>
-                        <button>Register</button>
+                    <input className="LoginPasswordInput LoginInput" type="text" id="LoginFormPassword" name="Password" placeholder="Password" style={{ marginBottom: "10px", background: "transparent"}} /> <br />
+                    <div className="LoginButtons" style={{ display: "flex", justifyContent: "space-between" }}>
+                        <button style={{ height: "30px", borderRadius: "10px", width: "120px" }}>Login</button>
+                        <button style={{ height: "30px", borderRadius: "10px", width: "120px" }}>Register</button>
                     </div>
-                    
                 </form>
             </div>
         </div>
