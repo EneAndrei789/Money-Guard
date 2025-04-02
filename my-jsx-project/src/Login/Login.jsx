6502a242e32605bcfd8d7a39231a682419c26d97
EneAndrei789 <andreileonardene@gmail.com>
2025-04-02 19:38:55 +0300

    completed the html part

--- my-jsx-project/src/Login/Login.jsx
+++ my-jsx-project/src/Login/Login.jsx
@@ -10,6 +10,11 @@ const Login = () => {
                     <input className="LoginEmailInput LoginInput" type="text" id="LoginFormEmail" name="Email" placeholder="Email" style={{ marginBottom: "10px" }} /> <br />
                     <label htmlFor="Password"></label>
                     <input className="LoginPasswordInput LoginInput" type="text" id="LoginFormPassword" name="Password" placeholder="Password" style={{ marginBottom: "10px" }} /> <br />
+                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                        <button>Login</button>
+                        <button>Register</button>
+                    </div>
+                    
                 </form>
             </div>
         </div>
