f4f7eae7eda98c523415d310f22335f61660cc84
EneAndrei789 <andreileonardene@gmail.com>
2025-04-02 20:31:25 +0300

    50% of login styles done

--- my-jsx-project/src/Login/Login.jsx
+++ my-jsx-project/src/Login/Login.jsx
@@ -7,12 +7,42 @@ const Login = () => {
             <div className="LoginForm">
                 <form action="">
                     <label htmlFor="Email"></label>
-                    <input className="LoginEmailInput LoginInput" type="text" id="LoginFormEmail" name="Email" placeholder="Email" style={{ marginBottom: "10px", background: "transparent"}} /> <br />
+                    <input
+                        className="LoginEmailInput LoginInput"
+                        type="text"
+                        id="LoginFormEmail"
+                        name="Email"
+                        placeholder="Email"
+                        style={{ marginBottom: "10px", background: "transparent" }}/>
+                    <br/>
                     <label htmlFor="Password"></label>
-                    <input className="LoginPasswordInput LoginInput" type="text" id="LoginFormPassword" name="Password" placeholder="Password" style={{ marginBottom: "10px", background: "transparent"}} /> <br />
-                    <div className="LoginButtons" style={{ display: "flex", justifyContent: "space-between" }}>
-                        <button style={{ height: "30px", borderRadius: "10px", width: "120px" }}>Login</button>
-                        <button style={{ height: "30px", borderRadius: "10px", width: "120px" }}>Register</button>
+                    <input
+                        className="LoginPasswordInput LoginInput"
+                        type="text"
+                        id="LoginFormPassword"
+                        name="Password"
+                        placeholder="Password"
+                        style={{ marginBottom: "10px", background: "transparent" }}/>
+                    <br/>
+                    <div
+                        className="LoginButtons"
+                        style={{ display: "flex", justifyContent: "space-between" }}>
+                        <button
+                            style={{
+                                height: "30px",
+                                borderRadius: "10px",
+                                width: "120px",
+                            }}>
+                            Login
+                        </button>
+                        <button
+                            style={{
+                                height: "30px",
+                                borderRadius: "10px",
+                                width: "120px",
+                            }}>
+                            Register
+                        </button>
                     </div>
                 </form>
             </div>
